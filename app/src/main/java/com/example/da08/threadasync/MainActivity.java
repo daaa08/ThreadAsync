00b9ac1af48b1593b8bf2b9ce45bf06634357737
박다정 <daaa08@naver.com>
2017-06-12 13:35:26 +0900

    AsyncTask 활용

--- app/src/main/java/com/example/da08/threadasync/MainActivity.java
+++ app/src/main/java/com/example/da08/threadasync/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.da08.threadasync;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -36,12 +37,12 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                run();
+                runAsync();
             }
         });
 
         // 정의
-        progress = new ProgressDialog(this);  // getBaseContext를 쓰면 MainActivity에 대한 자원만 가져오므로 에러가 발생 
+        progress = new ProgressDialog(this);  // getBaseContext를 쓰면 MainActivity에 대한 자원만 가져오므로 에러가 발생
         progress.setTitle("진행중");
         progress.setMessage("룰루랄라");
         progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
@@ -50,6 +51,52 @@ public class MainActivity extends AppCompatActivity {
     private void setDone(){
         textView.setText("Done");
         progress.dismiss(); // 해제
+    }
+
+    private void runAsync(){  // AsyncTask 활용
+        new AsyncTask< String, Integer, Float >(){
+            /* 제네릭 타입 1 - doInBackground 의 인자
+                        2 - onProgressUpdate 의 인자
+                        3 - doInBackground 의 리턴타입 */
+
+            // doInBackground 호출되기 전에 먼저 호출
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progress.show();
+            }
+
+            @Override
+            protected Float doInBackground(String... params) { // thread의 run과 같음
+                try {
+                    for(int i=0 ; i<10 ; i++) {
+                        publishProgress(i*10); // onProgressUpdate 를 주기적으로 업데이트 해준다.
+                        Thread.sleep(1000);
+                    }
+
+                    Thread.sleep(10000);
+                    handler.sendEmptyMessage(MainActivity.SET_DONE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return 1000.4f;
+            }
+
+            // doInBackground 호출되고나서 호출됨
+            @Override
+            protected void onPostExecute(Float result) {
+//                super.onPostExecute(aVoid);
+                // 결과값을 UI에 세팅
+                setDone();
+                progress.dismiss();
+            }
+
+            // 주기적으로 doInBackground 에서 호출이 가능한 함수
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                progress.setMessage("진행율 " +values[0]+ "%");
+            }
+        }.execute("a","b"); // thread의 start와 같은 의미  (doInBackground 실행)
     }
 
 
